57dbec56664347884301d972ae1b4fe3ee53be4f
Toxy <fluentoxdev17@gmail.com>
2024-04-04 16:10:31 +0200

    suppressing this warning for now

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -5,5 +5,6 @@ import javax.swing.JFrame;
 public class GUI {
     private static final String title = "Währungsrechner";
     private static final String version = "1.0_alpha";
+    @SuppressWarnings("unused") // temp, remove this line later
     private static JFrame frame = new JFrame(title + " " + version);
 }
