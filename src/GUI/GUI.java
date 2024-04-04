6b00a591103640d927285cbc110c6ca5d9966494
Toxy <fluentoxdev17@gmail.com>
2024-04-04 16:05:24 +0200

    some GUI stuff

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,5 +1,9 @@
 package GUI;
 
+import javax.swing.JFrame;
+
 public class GUI {
-    // ..
+    private static final String title = "Währungsrechner";
+    private static final String version = "1.0_alpha";
+    private static JFrame frame = new JFrame(title + " " + version);
 }
