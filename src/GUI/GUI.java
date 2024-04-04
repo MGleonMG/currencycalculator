2637f52e6a3861e0a04f0138b5cf6a0268c9210f
Toxy <fluentoxdev17@gmail.com>
2024-04-04 22:17:39 +0200

    cleanup

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -7,5 +7,4 @@ public class GUI {
     private static final String version = "1.0_alpha";
     @SuppressWarnings("unused") // temp, remove this line later
     private static JFrame frame = new JFrame(title + " " + version);
-    //nig
 }
--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -3,6 +3,5 @@ package Main;
 public class CurrencyCalculator {
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("huhn");
     }
 }
