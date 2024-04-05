00799bdb71c4000584212e4b0df0f4e902f2ac51
Leon <fluentoxdev17@gmail.com>
2024-04-05 11:16:09 +0200

    fixing stuff

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -6,59 +6,43 @@ import java.awt.*;
 import com.formdev.*;
 import com.formdev.flatlaf.FlatDarkLaf;
 
-
 public class GUI {
-    private static final String title = "Währungsrechner";
-    private static final String version = "1.0_alpha";
-    @SuppressWarnings("unused") // temp, remove this line later
-    private static JFrame frame = new JFrame(title + " " + version);
-    
-    
-    public static void main(String[] args) {
-    try{
-        UIManager.setLookAndFeel(new FlatDarkLaf());
-    }   catch (Exception e){
-        e.printStackTrace();
-    }
+    public static final String title = "Währungsrechner";
+    public static final String version = "1.0_alpha";
 
-        JFrame frame = new JFrame("Währungsumrechner"); // Titel setzen
+    public static void createGUI() {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JFrame frame = new JFrame(title + " " + version);
         frame.setSize(1000, 1000); // Größe setzen
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Beenden beim Schließen
         frame.setResizable(false);
 
-     
-        
-
-
         JLabel label = new JLabel("Währungsrechner");
-        label.setSize(new Dimension (100,30));
+        label.setSize(new Dimension(100, 30));
         label.setFont(label.getFont().deriveFont(30f));
-        label.setBounds(350,25,1000,50);
-       
+        label.setBounds(350, 25, 1000, 50);
 
         JButton MenuBtn = new JButton("Menü");
         MenuBtn.setSize(new Dimension(10, 10));
-        MenuBtn.setBounds(860,920,100,30);
+        MenuBtn.setBounds(860, 920, 100, 30);
 
-        JLabel build = new JLabel("v1.0 alpha by Leon, Jonas, Ewin" );
-        build.setBounds(10,900,500,100);
-      
+        JLabel build = new JLabel("v1.0 alpha by Leon, Jonas, Ewin");
+        build.setBounds(10, 900, 500, 100);
+
         build.setForeground(Color.GRAY);
-        
-      
-
-       
-        
-        
 
         // Das Panel zum Frame hinzufügen
-    
+
         frame.setLayout(null);
         frame.add(build);
         frame.add(label);
         frame.add(MenuBtn);
-        
 
         frame.setVisible(true); // Frame sichtbar machen
     }
--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -1,14 +1,12 @@
 package Main;
 
-
 import GUI.GUI;
 import Menu.Menu;
 
 public class CurrencyCalculator {
     public static void main(String[] args) {
-        System.out.println("hi");
-        new GUI();
+        System.out.println("\n\nStarting...  " + GUI.title + " (" + GUI.version + ")");
+        GUI.createGUI();
         new Menu();
-
     }
 }
