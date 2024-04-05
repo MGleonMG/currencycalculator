ba0e4a3665da00bf10a490ad364663d5cc78cbf0
Krüger <jkrueger@kloeschinski.de>
2024-04-05 09:13:45 +0200

    gui angefangen

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,10 +1,29 @@
 package GUI;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
-    private static final String title = "Währungsrechner";
-    private static final String version = "1.0_alpha";
-    @SuppressWarnings("unused") // temp, remove this line later
-    private static JFrame frame = new JFrame(title + " " + version);
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Währungsumrechner"); // Titel setzen
+        frame.setSize(1000, 1000); // Größe setzen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Beenden beim Schließen
+
+        // JPanel erstellen
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        JButton button = new JButton("Button"); // Button erstellen
+        // Setzen der bevorzugten Größe des Buttons auf 100x100 Pixel
+        button.setPreferredSize(new Dimension(100, 100));
+        // Hintergrundfarbe des Buttons auf Schwarz setzen
+        button.setBackground(Color.BLACK);
+        // Den Button zum Panel hinzufügen
+        panel.add(button);
+
+        // Das Panel zum Frame hinzufügen
+        frame.getContentPane().add(panel);
+
+        frame.setVisible(true); // Frame sichtbar machen
+    }
 }
--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -1,7 +1,11 @@
 package Main;
 
+import GUI.GUI;
+
 public class CurrencyCalculator {
     public static void main(String[] args) {
         System.out.println("hi");
+        new GUI();
+        
     }
 }
