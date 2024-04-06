20b474c181da5a6ea51de454fd791de06df86397
Toxy <fluentoxdev17@gmail.com>
2024-04-06 14:08:53 +0200

    built backend to fetch data and convert currencies

--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -1,5 +1,9 @@
 package Main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import Data.Calculations;
 import GUI.GUI;
 import Menu.Menu;
 
@@ -8,5 +12,29 @@ public class CurrencyCalculator {
         System.out.println("\n\nStarting...  " + GUI.title + " (" + GUI.version + ")");
         GUI.createGUI();
         new Menu();
+
+        testCurrencyBackend();
+    }
+
+    private static void testCurrencyBackend() {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        double amount;
+        String cur1, cur2;
+
+        // while (true) {
+        System.out.println("\nWelche Währung möchtest du konvertieren?  (zur Zeit Möglich: EUR, USD)");
+        cur1 = input.nextLine();
+
+        System.out.println("..und zu welcher Währung?");
+        cur2 = input.nextLine();
+
+        System.out.println("Wieviel " + cur1 + " möchtest du konvertieren? (z.B. '20.30')");
+        amount = input.nextDouble();
+
+        System.out.println("next double is " + amount);
+
+        Calculations.convertCurrencies(cur1, cur2, amount);
+        // }
     }
 }
