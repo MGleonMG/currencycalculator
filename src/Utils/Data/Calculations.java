0b6fd13af20d9d3728bc20bf2506f4eede37696d
Toxy <fluentoxdev17@gmail.com>
2024-04-08 20:10:40 +0200

    hotfix for convertCurrencies not returning anything

--- src/Utils/Data/Calculations.java
+++ src/Utils/Data/Calculations.java
@@ -3,19 +3,13 @@ package Utils.Data;
 import Utils.Utils;
 
 public class Calculations {
-    public static void convertCurrencies(String baseCur, String targetCur, double amount) {
+    public static double convertCurrencies(String baseCur, String targetCur, double amount) {
         ExchangeRateFetcher.fetchExchangeRate(baseCur, targetCur);
 
         if (ExchangeRateFetcher.latestRate != 0.0) {
-            System.out.println("\n========================================================================\n" +
-                    amount + " " + baseCur + " sind " +
-                    Utils.adjustDecimal(amount * ExchangeRateFetcher.latestRate, 2) + " " + targetCur +
-                    " bei einem Wechselkurs von " + ExchangeRateFetcher.latestRate +
-                    "\n========================================================================");
-
+            return Utils.adjustDecimal(amount * ExchangeRateFetcher.latestRate, 2);
         } else {
-            System.err.println("Ein Fehler ist aufgetreten...\nProgramm wird beendet.");
-            System.exit(0);
+            return 0.0;
         }
     }
 }
