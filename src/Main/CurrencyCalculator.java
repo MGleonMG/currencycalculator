8e6082666db85230bfeeb832aef77c1edc06ab1d
Krüger <jkrueger@kloeschinski.de>
2024-04-09 10:13:19 +0200

    Dropdown

--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -11,17 +11,10 @@ public class CurrencyCalculator {
 
     // Erstellt eine Dropdown Liste der Currency 
     // spezifisch fuer einen String -> vermeidet somit Fehler
-    static JComboBox<String> dropdown = new JComboBox<>();
 
     public static void main(String[] args) {
         GUI.drawGUI();
-    
-        for (Map.Entry<String, String> currency : Utils.getAllCurrencies()) {
-         String isoCode = currency.getKey();
-         String currencyName = currency.getValue();
 
-         dropdown.addItem(currencyName + " (" + isoCode + ")");
-         }
 
         // TODO: remove later
         // am besten nochmal gemeinsam klaeren, wie wir das durchsetzen
