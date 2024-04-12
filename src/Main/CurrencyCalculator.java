2c9b2dd8fd52d64a43af9d9f10a6be149a009d60
Toxy <fluentoxdev17@gmail.com>
2024-04-12 20:49:42 +0200

    stashing..

--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -5,33 +5,34 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 
 import GUI.GUI;
+import GUI.Errors.ErrorDisplay;
 import Utils.Data.Config.Config;
 import Utils.Data.Config.SavedConfigs;
 
 public class CurrencyCalculator {
+    public static String folderPath = System.getProperty("user.home") + FileSystems.getDefault().getSeparator()
+            + "currencycalculator";
+    public static String filePath = folderPath + FileSystems.getDefault().getSeparator() + "settings.json";
 
     public static void main(String[] args) {
         GUI.drawGUI();
 
-        // String filePath = System.getProperty("user.home") + FileSystems.getDefault().getSeparator()
-        //         + "currencycalculator" + FileSystems.getDefault().getSeparator() + "settings.json";
+        try {
+            new File(folderPath).mkdir();
+            new File(filePath).createNewFile();
 
-        // System.out.println(filePath);
+            SavedConfigs.addToJson("testKey", "testValue");
 
-        // try {
-        //     if (filePath == null) {
-        //         new File(filePath).createNewFile();
-        //     }
+            String json = Config.readJSONFile(filePath);
+            String key = SavedConfigs.parseKey(json);
+            String value = SavedConfigs.parseValue(json);
+            System.out.println("Key: " + key);
+            System.out.println("Value: " + value);
 
-        //     String json = Config.readJSONFile(filePath);
-        //     String key = SavedConfigs.parseKey(json);
-        //     String value = SavedConfigs.parseValue(json);
-        //     System.out.println("Key: " + key);
-        //     System.out.println("Value: " + value);
-
-        // } catch (IOException e) {
-
-        //     e.printStackTrace();
-        // }
+        } catch (IOException e) {
+            e.printStackTrace();
+            ErrorDisplay.throwErrorPopup(e.getMessage());
+            // TODO: write a proper error
+        }
     }
 }
--- src/Utils/Data/Config/SavedConfigs.java
+++ src/Utils/Data/Config/SavedConfigs.java
@@ -1,5 +1,9 @@
 package Utils.Data.Config;
 
+import java.io.IOException;
+
+import Main.CurrencyCalculator;
+
 public class SavedConfigs {
 
     public static String parseKey(String json) {
@@ -13,8 +17,19 @@ public class SavedConfigs {
         int endIndex = json.indexOf('"', startIndex);
         return json.substring(startIndex, endIndex);
     }
-    
+
+    public static String addToJson(String key, String value) throws IOException {
+        // Remove the last '}' from JSON
+        String json = Config.readJSONFile(CurrencyCalculator.filePath).substring(0, Config.readJSONFile(CurrencyCalculator.filePath).length() - 1);
+
+        // Add new key-value pair
+        json += ", \"" + key + "\": \"" + value + "\"}";
+
+        return json;
+    }
+
+
     // public static Config loadConfig(String pathString) /* throws some Exc??*/ {
-    //     return null; // temp
+    // return null; // temp
     // }
 }
