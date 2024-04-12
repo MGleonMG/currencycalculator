f41151550c8672ed4020b4473b71049a7716f4d0
Leon <fluentoxdev17@gmail.com>
2024-04-12 15:31:17 +0200

    added clearDataOnError function, changed the way the webScanner was set up, closed scanner when finished

--- src/Utils/Data/ExchangeRateFetcher.java
+++ src/Utils/Data/ExchangeRateFetcher.java
@@ -12,49 +12,53 @@ public class ExchangeRateFetcher {
     private static long lastStartMillis, lastEndMillis;
 
     // Webscraper um Daten von 'google.com/finance/' zu nutzen
-    // TODO: @Leon close scanner
-    @SuppressWarnings({ "resource" })
     public static void fetchExchangeRate(String baseCur, String targetCur) {
         try {
-            System.out.println("Fetching Exchange rate...");
             lastStartMillis = System.currentTimeMillis();
-            // Roher HTML code als String von 'google.com/finance/'
-            String htmlOutput = new Scanner(
-                    new URL("https://www.google.com/finance/quote/" + baseCur + "-" + targetCur).openStream(), "UTF-8")
-                    .useDelimiter("\\A").next();
 
-            // Stelle finden an der der Wechselkurs angegeben ist und den String darauf
-            // zuschneiden
+            // Roher HTML code als String von 'google.com/finance/'
+            @SuppressWarnings("deprecation")
+            Scanner webScanner = new Scanner(new URL("https://www.google.com/finance/quote/" + baseCur + "-" + targetCur).openStream(), "UTF-8");
+            String htmlOutput = webScanner.useDelimiter("\\A").next();
+
+            // Stelle finden an der der Wechselkurs angegeben ist
+            // und den String darauf zuschneiden
             htmlOutput = htmlOutput.substring(htmlOutput.indexOf("YMlKec fxKbKc\">"));
             htmlOutput = htmlOutput.substring(15, htmlOutput.indexOf("<"));
 
             lastEndMillis = System.currentTimeMillis();
 
-            System.out.println("Exchange rate fetched within " + (getLastFetchTime()) + "ms");
             latestRate = Utils.adjustDecimal(Double.parseDouble(htmlOutput), 4);
 
+            webScanner.close();
         } catch (UnknownHostException uhExc) {
             ErrorDisplay.throwErrorPopup("Es konnte keine Verbindung zum Server hergestellt werden");
             uhExc.printStackTrace();
-            latestRate = 0.0;
+            clearDataOnError();
 
         } catch (StringIndexOutOfBoundsException oobExc) {
             // Fehler aufgetreten.
             ErrorDisplay.throwErrorPopup(
                     "Einer der Währungen scheint nicht zu existieren oder es ist ein Fehler beim fetchen der Daten aufgetreten");
             oobExc.printStackTrace();
-            latestRate = 0.0;
+            clearDataOnError();
 
         } catch (Exception exc) {
             System.err.print("[UNKNOWN ERROR] ");
             ErrorDisplay.throwErrorPopup("Ein unbekannter Fehler ist aufgetreten");
             exc.printStackTrace();
-            latestRate = 0.0;
+            clearDataOnError();
 
         }
     }
-        
+
     public static long getLastFetchTime() {
         return lastEndMillis - lastStartMillis;
+    }
+
+    private static void clearDataOnError() {
+        latestRate = 0.0;
+        lastStartMillis = 0;
+        lastEndMillis = 0;
     }
 }
