f29f0e01934cad007211c7dfa2ca6f116e98f373
Leon <fluentoxdev17@gmail.com>
2024-04-18 10:05:25 +0200

    created constructor

---
+++ src/Utils/Data/Config/Settings/LastCalculation.java
@@ -0,0 +1,13 @@
+package Utils.Data.Config.Settings;
+
+public class LastCalculation {
+    String baseCur, targetCur, amount;
+    //private static final String key = "?";
+
+    public LastCalculation(String baseCur, String targetCur, String amount) {
+        this.baseCur = baseCur;
+        this.targetCur = targetCur;
+        this.amount = amount;
+    }
+
+}
