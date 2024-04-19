cc7398a7970c3f597f59ec14dcd273db43bcc9a4
Toxy <fluentoxdev17@gmail.com>
2024-04-19 20:26:17 +0200

    fixed some comments

--- src/GUI/Popups/PopupDisplay.java
+++ src/GUI/Popups/PopupDisplay.java
@@ -3,14 +3,15 @@ package GUI.Popups;
 import javax.swing.JOptionPane;
 
 /*
- * Diese Klasse erstellt eine Fehlermeldung und gibt dies dem Enduser zurück
+ * Diese Klasse erstellt eine Fehlermeldungs Popups und Info Popups 
+ * und gibt dies dem Enduser zurück
  */
-public class PopupDisplay { //TODO: @Leon refactor class name to something more fitting
+public class PopupDisplay {
     public static void throwErrorPopup(String errorMsg) {
         JOptionPane.showMessageDialog(null, errorMsg, "Ein Fehler ist aufgetreten", JOptionPane.ERROR_MESSAGE);
     }
 
-    public static void throwInfoPopup(String title, String msg) { // TODO: @Leon check if looks good
+    public static void throwInfoPopup(String title, String msg) {
         JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
     }
 }
