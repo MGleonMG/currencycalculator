5cf2a728d5236fdc93b032e115c68cd3b7997b0f
Leon <fluentoxdev17@gmail.com>
2024-04-19 14:34:57 +0200

    new function

--- src/GUI/Errors/ErrorDisplay.java
+++ src/GUI/Errors/ErrorDisplay.java
@@ -5,8 +5,12 @@ import javax.swing.JOptionPane;
 /*
  * Diese Klasse erstellt eine Fehlermeldung und gibt dies dem Enduser zurück
  */
-public class ErrorDisplay {
+public class ErrorDisplay { //TODO: @Leon refactor class name to something more fitting
     public static void throwErrorPopup(String errorMsg) {
         JOptionPane.showMessageDialog(null, errorMsg, "Ein Fehler ist aufgetreten", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void throwInfosPopup(String title, String msg) { // TODO: @Leon check if looks good
+        JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
     }
 }
