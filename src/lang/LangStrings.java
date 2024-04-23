3a57e9471866b56dc8c61acc4573808006decbdf
Ewin <ewinl21@gmail.com>
2024-04-23 09:06:00 +0200

    added more Languages, added Loading GIF, disabled Language Method

--- src/lang/LangStrings.java
+++ src/lang/LangStrings.java
@@ -16,6 +16,10 @@ public class LangStrings {
      * - Könnt gerne noch Sprache(n) hinzufügen aber kein Französisch!! >:(
      */
 
+    /*
+     * TODO Sollte vielleicht "%TARGET_ISO% sein?" 
+     */
+
     // Deutsch
     public static final String GER_TITLE = "Währungsrechner";
     public static final String GER_BTN_CALC = "Umrechnen";
@@ -23,7 +27,26 @@ public class LangStrings {
             + "\nDas Ergebnis ist %RESULT% %TARGET_ISO"
             + "\nDie Operation dauerte %FETCH_TIME%ms";
 
-    // Englisch
+    // English
     public static final String ENG_TITLE = "Currencycalculator";
     public static final String ENG_BTN_CALC = "Convert";
+    public static final String ENG_LBL_OUTPUT = "Typed: %INPUT %BASE_ISO%"
+            + "\nThe Results are %RESULT %TARGET_ISO"
+            + "\nThe Operation took %FETCH_TIME%ms";
+
+    // Español
+
+    public static final String ESP_TITLE = "Calculadora de Monedas";
+    public static final String ESP_BTN_CALC = "Convertir";
+    public static final String ESP_LBL_OUTPUT = "Mecanografiado: %INPUT %BASE_ISO"
+            + "\nLos resultados son %RESULT %TARGET_ISO"
+            + "\nLa operación tomó %FETCH_TIME%ms";
+
+    // Danish
+
+    public static final String DAN_TITLE = "Valutaberegner";
+    public static final String DAN_BTN_CALC = "Konvertere";
+    public static final String DAN_LBL_OUTPUT = "Indtastet: %INPUT %BASE_ISO%"
+            + "\nResultaterne er %RESULT %TARGET_ISO"
+            + "\nOperationen tog %FETCH_TIME%ms";
 }
