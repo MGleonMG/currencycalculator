3c22c46f6e04ee6a315f1052e0de90eeaa294013
Syhnx <108582941+Syhnx@users.noreply.github.com>
2024-04-24 14:08:48 +0200

    hotfix

--- src/lang/LangStrings.java
+++ src/lang/LangStrings.java
@@ -27,15 +27,15 @@ public class LangStrings {
     public static final String ENG_TITLE = "Currencycalculator";
     public static final String ENG_BTN_CALC = "Convert";
     public static final String ENG_LBL_OUTPUT = "Typed: %INPUT% %BASE_ISO%"
-            + "\nThe Results are %RESULT %TARGET_ISO%"
+            + "\nThe Results are %RESULT% %TARGET_ISO%"
             + "\nThe Operation took %FETCH_TIME%ms";
 
     // Español
 
     public static final String ESP_TITLE = "Calculadora de Monedas";
     public static final String ESP_BTN_CALC = "Convertir";
-    public static final String ESP_LBL_OUTPUT = "Mecanografiado: %INPUT %BASE_ISO%"
-            + "\nLos resultados son %RESULT %TARGET_ISO%"
+    public static final String ESP_LBL_OUTPUT = "Mecanografiado: %INPUT% %BASE_ISO%"
+            + "\nLos resultados son %RESULT% %TARGET_ISO%"
             + "\nLa operación tomó %FETCH_TIME%ms";
 
     // Danish
@@ -43,6 +43,6 @@ public class LangStrings {
     public static final String DAN_TITLE = "Valutaberegner";
     public static final String DAN_BTN_CALC = "Konvertere";
     public static final String DAN_LBL_OUTPUT = "Indtastet: %INPUT% %BASE_ISO%"
-            + "\nResultaterne er %RESULT %TARGET_ISO%"
+            + "\nResultaterne er %RESULT% %TARGET_ISO%"
             + "\nOperationen tog %FETCH_TIME%ms";
 }
