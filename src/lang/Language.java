3cb48649c3c87ca608b742f74f368885c5d85aae
Toxy <fluentoxdev17@gmail.com>
2024-05-01 19:47:22 +0200

    refactored lang strings into lang packages

--- src/lang/Language.java
+++ src/lang/Language.java
@@ -1,12 +1,15 @@
 package lang;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import Utils.Data.Config.Settings.AppLanguage;
 
 public class Language {
     private static Locale locale;
-    // ..
+    private static ResourceBundle langBundle;
+
     // Auflistung aller unterstützten Sprachen
     public enum Languages {
         GERMAN,
@@ -17,48 +20,53 @@ public class Language {
 
     /*
      * Funktion um die Anzeige-Sprache zu ändern.
-     * "language" nimmt ein eine Auflistung namens Languages entgegen
-     * "updateConfig" nimmt einen boolean entgegen der bestimmt ob eine Änderung in
-     * der config Datei des benutzers nötig ist
+     * - "language" nimmt ein eine Auflistung namens Languages entgegen
+     * - "updateConfig" nimmt einen boolean entgegen der bestimmt ob eine Änderung
+     * in der config Datei des benutzers nötig ist
      */
+    @SuppressWarnings("deprecation")
     public static void setAppLanguage(Languages language, boolean updateConfig) {
         switch (language) {
             case ENGLISH:
-                locale = Locale.US;
-                Locale.setDefault(locale);
-                if (updateConfig) {
-                    AppLanguage.setConfigAppLanguage(Languages.ENGLISH);
-                }
+                locale = Locale.ENGLISH;
                 break;
 
             case GERMAN:
-                locale = Locale.GERMANY;
-                Locale.setDefault(locale);
-                AppLanguage.setConfigAppLanguage(Languages.GERMAN);
-                if (updateConfig) {
-                    AppLanguage.setConfigAppLanguage(Languages.GERMAN);
-                }
+                locale = new Locale("de", "DE");
                 break;
 
             case SPANISH:
-                // TODO: Spanish missing in Locale class? Check later..
-                // locale = Locale.;
-                Locale.setDefault(locale);
-                AppLanguage.setConfigAppLanguage(Languages.SPANISH);
-                if (updateConfig) {
-                    AppLanguage.setConfigAppLanguage(Languages.SPANISH);
-                }
+                locale = new Locale("es", "ES");
                 break;
 
             case DANISH:
-                // TODO: same here as above??
-                // locale = Locale.DANISH;
-                Locale.setDefault(locale);
-                AppLanguage.setConfigAppLanguage(Languages.DANISH);
-                if (updateConfig) {
-                    AppLanguage.setConfigAppLanguage(Languages.DANISH);
-                }
+                locale = new Locale("da", "DK");
                 break;
         }
+
+        if (updateConfig) {
+            AppLanguage.setConfigAppLanguage(language);
+        }
+
+        Locale.setDefault(locale);
+        System.out.println("Default locale: " + Locale.getDefault());
+
+        String bundleName = "lang_" + language.name().toLowerCase() + ".properties";
+        System.out.println("Bundle name: " + bundleName);
+
+        langBundle = ResourceBundle.getBundle(bundleName, locale);
+
+        try {
+            langBundle = ResourceBundle.getBundle(bundleName, locale);
+            System.out.println("Bundle loaded successfully!");
+        } catch (MissingResourceException e) {
+            System.err.println("Failed to load bundle: " + e.getMessage());
+        }
+    }
+
+    public static String getLangStringByKey(String key) {
+        // langBundle.
+
+        return "ABC";
     }
 }
