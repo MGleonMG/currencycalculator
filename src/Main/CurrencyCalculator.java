93544c35ee9470a95db944de70489921b64e67ab
Toxy <fluentoxdev17@gmail.com>
2024-05-01 20:14:50 +0200

    stashing..

--- src/Main/CurrencyCalculator.java
+++ src/Main/CurrencyCalculator.java
@@ -1,18 +1,23 @@
 package Main;
 
+import java.io.IOException;
+
 import GUI.GUI;
 import Utils.Data.Config.Config;
 import Utils.Data.Config.Settings.AppLanguage;
 import lang.Language;
+import lang.Language.Languages;
 
 /*
  * Dies ist die HauptKlasse
  */
 public class CurrencyCalculator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Config.runFirstTimeSetupCheck();
-        Language.setAppLanguage(AppLanguage.getConfigAppLanguage(), false);
+        // Language.setAppLanguage(AppLanguage.getConfigAppLanguage(), false);
+        Language.setAppLanguage(Languages.SPANISH, false);
         GUI.drawGUI();
+        System.out.println("\"" + Language.getLangStringByKey("title") + "\"");
     }
 }
--- src/lang/Language.java
+++ src/lang/Language.java
@@ -1,14 +1,18 @@
 package lang;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 import Utils.Data.Config.Settings.AppLanguage;
+import lang.Language.Languages;
 
 public class Language {
     private static Locale locale;
-    private static ResourceBundle langBundle;
+    private static Properties langBundle;
 
     // Auflistung aller unterstützten Sprachen
     public enum Languages {
@@ -25,48 +29,42 @@ public class Language {
      * in der config Datei des benutzers nötig ist
      */
     @SuppressWarnings("deprecation")
-    public static void setAppLanguage(Languages language, boolean updateConfig) {
+    public static void setAppLanguage(Languages language, boolean updateConfig) throws IOException {
+//         String fileName = "/languages/lang_" + language.name().toLowerCase() + ".properties";
+// InputStream inputStream = Language.class.getResource(fileName).openStream();
+    
+        Properties properties = new Properties();
+        //properties.load(inputStream);
+    
         switch (language) {
             case ENGLISH:
                 locale = Locale.ENGLISH;
                 break;
-
+    
             case GERMAN:
                 locale = new Locale("de", "DE");
                 break;
-
+    
             case SPANISH:
                 locale = new Locale("es", "ES");
                 break;
-
+    
             case DANISH:
                 locale = new Locale("da", "DK");
                 break;
         }
-
+    
+        Locale.setDefault(locale);
+    
         if (updateConfig) {
             AppLanguage.setConfigAppLanguage(language);
         }
-
-        Locale.setDefault(locale);
-        System.out.println("Default locale: " + Locale.getDefault());
-
-        String bundleName = "lang_" + language.name().toLowerCase() + ".properties";
-        System.out.println("Bundle name: " + bundleName);
-
-        langBundle = ResourceBundle.getBundle(bundleName, locale);
-
-        try {
-            langBundle = ResourceBundle.getBundle(bundleName, locale);
-            System.out.println("Bundle loaded successfully!");
-        } catch (MissingResourceException e) {
-            System.err.println("Failed to load bundle: " + e.getMessage());
-        }
+    
+        langBundle = properties;
     }
+    
 
     public static String getLangStringByKey(String key) {
-        // langBundle.
-
-        return "ABC";
+        return langBundle.getProperty(key);
     }
 }
