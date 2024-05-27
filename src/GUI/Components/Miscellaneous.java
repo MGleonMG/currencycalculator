4419c16c15e0919b8c84a7ad88edc2e6f3544d66
Ewin <ewinl21@gmail.com>
2024-05-27 17:24:25 +0200

    Fixed mistake, added some Miscallaneous

--- src/GUI/Components/Miscellaneous.java
+++ src/GUI/Components/Miscellaneous.java
@@ -1,5 +1,22 @@
 package GUI.Components;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
 public class Miscellaneous {
-    
+
+    private static JButton saveBtn = new JButton();
+    private static JButton loadBtn = new JButton();
+    private static JLabel presetLabel = new JLabel();
+    private static JLabel fadeLabel = new JLabel();
+    private static JLabel clipboardLblBtn = new JLabel();
+
+    public static void addAllComponents() {
+
+        setComponentBounds();
+    }
+
+    private static void setComponentBounds() {
+
+    }
 }
